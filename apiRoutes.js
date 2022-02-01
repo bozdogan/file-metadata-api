54167a5e539395e911213f71144f45a2ed2d01fd
Bozdogan <boraozdogan99@gmail.com>
2022-02-01 23:33:13 +0300

    impl. api routes

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const multer = require("multer");
 
+const upload = multer({dest: "uploads/"});
 const router = express.Router();
 
-router.post("/", (req, res) => {
-    res.send("API root");
+router.post("/", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.json({
+        mime: file.mimetype,
+        size: file.size
+    });
 });
 
 module.exports = router;
